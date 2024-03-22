8fd2e276c255e235b9fe2e780ecc0da9ab9357ce
longbytes <dsbuff01@gmail.com>
2024-03-21 20:06:45 -0600

    add javascript

---
+++ form-validator/script.js
@@ -0,0 +1,23 @@
+// Create variables
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+}
+
+// Add event listener
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    if(username.value === '') {
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
+});
